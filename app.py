f50bafc43559027d6103e5036a82d31fdefdb193
Mohd Bilal Hasan <bilalhasan403@gmail.com>
2024-11-21 20:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def analyze(input_text):
   clean_text = remove_stopwords(input_text)
   tokens = tokeinize_words(clean_text)
   word_count = compute_word_count(tokens)
-  sentence_count = (count_sentences(clean_text)
+  sentence_count = (count_sentences(clean_text))
   average_sentence_length = round(word_count / sentence_count)
   complex_word_count = compute_num_complex_words(tokens)
   percentage_of_complex_words = round((Complex_word_count / word_count) * 100, 2)
